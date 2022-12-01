099c60b8d07c8a56245adc1a864d7c5bf5da420c
stefan <stefan.egli@gmail.com>
2022-12-01 22:48:45 +0100

    solution to problem 2 day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,38 +1,37 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-
+use std::vec::Vec;
 
 fn main() {
     println!("Advent of code 2022, day 1");
 
-
-    // read in input from file
     let filename = "rsc/input.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
     let mut calories = 0;
-
     let mut max = 0;
+    let mut vec = Vec::new();
 
-    for (index, line) in reader.lines().enumerate() {
+    for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         if line.trim().is_empty() {
-            // println!("{}", "empty");
-            println!("{}", calories);
-            // create a new Elve
             if calories > max {
                 max = calories;
+                vec.push(calories);
             }
-
             calories = 0;
 
         } else {
             calories += line.parse::<i32>().unwrap();
         }
 
-        //println!("{}. {}", index+1, line);
     }
 
+    vec.sort();
+    vec.reverse();
+
+    let sum: i32 = vec[0..3].iter().sum();
     println!("max: {}", max);
+    println!("sum of 3 biggest: {}", sum);
 }
