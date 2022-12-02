3aacf0d7585f11c4311fbd971e7cb0f80cda0556
stefan <stefan.egli@gmail.com>
2022-12-02 15:06:07 +0100

    solution 2 day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -14,8 +14,38 @@ use std::io::{BufRead, BufReader};
 // Scissors Defeat Paper
 // Paper Defeats Rock
 // Same shape = draw
-//
-//
+fn evaluate_game_2(op: &str, my: &str) -> i32 {
+    match op {
+        "A" => {
+            match my { 
+                "X" => {3}, 
+                "Y" => {4},
+                "Z" => {8}, 
+                _ => {0} 
+            }
+        },
+        "B" => {
+            match my {
+                "X" => {1}, 
+                "Y" => {5}, 
+                "Z" => {9},
+                _ => {0} 
+            }
+        },
+        "C" => {
+            match my { 
+                "X" => {2},
+                "Y" => {6}, 
+                "Z" => {7}, 
+                _ => {0} }
+        },
+        _ => 0,
+    }    
+}
+
+
+
+
 fn evaluate_game(op: &str, my: &str) -> i32 {
     match op {
         "A" => {
@@ -53,8 +83,8 @@ fn main() {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut sum = 0;
-
+    let mut sum_1 = 0;
+    let mut sum_2 = 0;
 
     for (_index, line) in reader.lines().enumerate() {
         let line_uw = line.unwrap();
@@ -63,14 +93,19 @@ fn main() {
         let op_shape = split.next().unwrap();
         let my_shape = split.next().unwrap();
         
-        let result = evaluate_game(op_shape, my_shape);
-        //println!("op: {}, my: {}, result: {}", op_shape, my_shape, result);
-      
-        sum += result;
-        println!("sum: {}", sum);
+        let result_1 = evaluate_game(op_shape, my_shape);
+        let result_2 = evaluate_game_2(op_shape, my_shape);
+              
+        sum_1 += result_1;
+        sum_2 += result_2;
+
+        //println!("sum 1: {}", sum_1);
+        //println!("sum 2: {}", sum_2);
     }
 
-    println!("final sum: {}", sum);
+    println!("final sum 1: {}", sum_1);
+    println!("final sum 2: {}", sum_2);
+    
 
 
 
