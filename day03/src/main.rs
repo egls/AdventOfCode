73ec76a167e1e5cbefa98533441ae25420e67914
stefan <stefan.egli@gmail.com>
2022-12-03 20:00:05 +0100

    solution 2 day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,9 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 
-fn main() {
-    println!("Advent of code 2021, day 3!");
-
+fn problem_1() {
     let lookup = vec!["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
     let filename = "rsc/input.txt";
@@ -19,15 +17,15 @@ fn main() {
         let len = str.len() / 2;
         let subs = str.split_at(len);
 
-        println!("1: {}, 2: {}",subs.0,subs.1);
+        //println!("1: {}, 2: {}",subs.0,subs.1);
         // search through string for same character
         for c in subs.0.chars() {
 
             if subs.1.contains(c){
-                println!("subs.1 contains: {}", c);
+                //println!("subs.1 contains: {}", c);
                 let cl = c.to_lowercase();
                 let mut index = lookup.iter().position(|&elem| elem == cl.to_string() ).unwrap() + 1;
-                print!(" index: {}\n", index);
+                //print!(" index: {}\n", index);
                 if c.is_uppercase() {
                     index += 26;
                 }
@@ -36,8 +34,51 @@ fn main() {
             }
 
         }
-        
-        
+    }
+     println!("sum: {}", sum);
+}
+
+fn main() {
+    println!("Advent of code 2021, day 3!");
+    problem_1();
+
+    let lookup = vec!["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+    let filename = "rsc/input.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut sum = 0;
+    let mut group: Vec<String> = Vec::with_capacity(3);
+    let mut ctr = 1;
+    for (idx, line) in reader.lines().enumerate() {
+   
+        if ctr!=0 && ctr%3==0 {
+            group.push(line.unwrap());
+            // process
+            // println!("idx: {}, 1: {}, 2: {}, 3: {}", ctr, group[0].as_str(), group[1].as_str(), group[2].as_str());
+            
+            for c in group[0].chars(){
+                if group[1].contains(c) && group[2].contains(c){
+                    let cl = c.to_lowercase();
+                    let mut index = lookup.iter().position(|&elem| elem == cl.to_string() ).unwrap() + 1;
+                    print!("char: {}, index: {}\n", c, index);
+                    if c.is_uppercase() {
+                        index += 26;
+                    }
+                    sum += index;
+                    break;
+                }
+
+            }
+            
+            println!("sum group: {}", sum);
+            group.clear();            
+        } else {
+           
+            group.push(line.unwrap());
+        }
+        ctr+=1;
     }
     println!("sum: {}", sum);
 }
