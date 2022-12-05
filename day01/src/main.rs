4c0790f24f49a0a978b51713cf27e04b71d67b3c
stefan <stefan.egli@gmail.com>
2022-12-05 22:52:14 +0100

    cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -10,28 +10,19 @@ fn main() {
     let reader = BufReader::new(file);
 
     let mut calories = 0;
-    let mut max = 0;
     let mut vec = Vec::new();
 
     for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         if line.trim().is_empty() {
-            if calories > max {
-                max = calories;
-                vec.push(calories);
-            }
+            vec.push(calories);
             calories = 0;
-
         } else {
             calories += line.parse::<i32>().unwrap();
         }
-
     }
-
     vec.sort();
     vec.reverse();
-
-    let sum: i32 = vec[0..3].iter().sum();
-    println!("max: {}", max);
-    println!("sum of 3 biggest: {}", sum);
+    println!("max calories: {}", vec[0]);
+    println!("sum of 3 biggest: {}", vec[0..3].iter().sum::<i32>());
 }
